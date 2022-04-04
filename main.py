ca639fbcb8b7bd45304adf017bc80720328b771f
den10b <den10b@yandex.ru>
2022-04-05 00:54:47 +0300

    1

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from typing import Any
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import StatesGroup, State
-from aiogram.types import Message, CallbackQuery, ContentType
+from aiogram.types import Message, CallbackQuery, ContentType, ChatType
 
 from aiogram_dialog import Dialog, DialogManager, DialogRegistry, Window, ChatEvent, StartMode
 from aiogram_dialog.manager.protocols import ManagedDialogAdapterProto
@@ -19,11 +19,20 @@ src_dir = os.path.normpath(os.path.join(__file__, os.path.pardir))
 
 API_TOKEN = "5176288897:AAFWal8jXz6Z4SKPJYf4MNsxc5tRskDQRYY"
 CHAT_ID = "-721759162"
-DATA = []
+DATA = {}
 logging.basicConfig(level=logging.INFO)
 storage = MemoryStorage()
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot, storage=storage)
+
+
+class counter:
+    TOKEN = 110
+
+    @classmethod
+    def get_count(cls):
+        counter.TOKEN = counter.TOKEN + 1
+        return cls.TOKEN
 
 
 class DialogSG(StatesGroup):
@@ -34,13 +43,17 @@ class DialogSG(StatesGroup):
 
 async def qest_handler(m: Message, dialog: ManagedDialogAdapterProto,
                        manager: DialogManager):
-    x = await bot.send_message(CHAT_ID, m.text)
-    DATA.append(x.message_id)
+    count = counter.get_count()
+    await bot.send_message(CHAT_ID, m.text + " " + str(count))
+    DATA[count] = m.from_user.id
 
 
 async def answ_handler(m: Message, dialog: ManagedDialogAdapterProto,
                        manager: DialogManager):
-    await Bot(token=API_TOKEN).send_message(CHAT_ID, m.text)
+    keys = DATA.keys()
+    for i in keys:
+        if m.text.find(str(i)):
+            await bot.send_message(DATA[i], m.text)
 
 
 dialog = Dialog(
@@ -55,7 +68,7 @@ dialog = Dialog(
         state=DialogSG.ask
     ),
     Window(
-        Const("Greetings! Please, ask:"),
+        Const("Please, answer:"),
         MessageInput(answ_handler),
         state=DialogSG.admin
     )
@@ -69,12 +82,12 @@ async def start(m: Message, dialog_manager: DialogManager):
 
 async def admin(m: Message, dialog_manager: DialogManager):
     # it is important to reset stack because user wants to restart everything
-    await dialog_manager.start(DialogSG.admin, mode=StartMode.RESET_STACK)
+    await dialog_manager.start(DialogSG.admin, mode=StartMode.NORMAL)
 
 
 async def main():
     dp.register_message_handler(start, text="/start", state="*")
-    dp.register_message_handler(start, text="/admin", state="*")
+    dp.register_message_handler(admin, text="/admin", state="*")
     registry = DialogRegistry(dp)
     registry.register(dialog)
 
